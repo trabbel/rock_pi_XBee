71e2f5650dbd5376daacc9feb822ed129a9bf209
Tim-Lucas Rabbel <timlucas.rabbel@student.uni-luebeck.de>
2023-03-31 15:51:16 +0200

    Sending over serial pins

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from zigbee import writeFrame
+import serial
+import time
 
 string = "test~"
 payload = bytearray()
@@ -6,6 +8,12 @@ payload.extend(string.encode())
 print(payload)
 frame = bytearray(300)
 
+xbee = serial.Serial('/dev/ttyS1', 115200)
+
 length = writeFrame(frame, 0xFFFE, 0x0013a20041f217cc, payload, len(payload))
 print(frame[:length])
 print(length)
+
+while True:
+    xbee.write(frame[:length])
+    time.sleep(2)
